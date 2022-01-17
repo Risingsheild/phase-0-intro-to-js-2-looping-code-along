3935d3b07a02b312e2467816a9f2cb45784b27c9
nickwells <wellsnick11@yahoo.com>
2022-01-16 19:04:53 -0800

    commit tree 6f884f66f806b30ff79d7562ee2bffbbdce69c0f

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-function  writeCards([name]) {
-    return ["Thank you, Guadalupe, for the wonderful surprise gift!",
-    "Thank you, Ollie, for the wonderful surprise gift!",
-    "Thank you, Aki, for the wonderful surprise gift!"]
+function writeCards (namesArray){
+    let Cards = []
+    for (let i = 0; i <namesArray.length; i++){
+        Cards.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`)
+    }
+    return Cards
 }
-function countDown(i) {
-    while (i>=0) {console.log(i--);}
+function countDown(number){
+    while (number>0){
+        console.log(number);
+        number -=1
+    }
+    console.log(number);
 }
